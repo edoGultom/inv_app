6d99ba95670ed9f890b5d31492edb4e4e9752e16
edoGultom <edogultom10395@gmail.com>
2023-08-16 12:02:25 +0700

    env

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => '\yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=db_inventaris',
-            'username' => 'root',
-            'password' => 'mdb124',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=edogulto_sipbang',
+            'username' => 'mysql_sipbang',
+            'password' => 'sipbang2023',
             'charset' => 'utf8',
         ],
         'mailer' => [
